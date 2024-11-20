d90c25aa16ae61a691498630c59d0ce21162c7e6
Luke <monroe.luke36@gmail.com>
2024-11-20 14:27:11 -0500

    fixed bug with bombCard logic

--- src/BombCard.java
+++ src/BombCard.java
@@ -1,12 +1,10 @@
 package src;
-//Maybe have 2 Decoy cards i.e bombs, if you click both bombs, you lose
+
 import java.io.File;
-//also i dont have a bomb card image rn, 
 public class BombCard extends Card{
     private static File imagePathFile = ImageCache.getImageFile("CardSprites", "bomb", "png");
 
     public BombCard(GameManager game){
         super(imagePathFile, game);
-        addActionListener(e -> game.new CardClickHandler(this));
     }
 }
